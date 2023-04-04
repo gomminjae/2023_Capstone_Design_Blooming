42f1227268d41c6c372add5ab4e77b3d079770cf
kim-seonghyun <aruesin2@gmail.com>
2023-04-05 00:58:47 +0900

    backend/modelServer 추가(Spring Framework사용)

---
+++ BackENd/modelServer/src/main/java/com/blooming/modelServer/ModelServerApplication.java
@@ -0,0 +1,13 @@
+package com.blooming.modelServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelServerApplication.class, args);
+	}
+
+}
